03c61788556665ba5c8a428364d2e9901e1f4c4f
Joao Vissoci <joaovissoci@gmail.com>
2012-06-11 15:21:19 -0300

    Actualization of the script with insertion of Blend Altman plot

--- olecranonFracture.R
+++ olecranonFracture.R
@@ -13,7 +13,7 @@ detach()
 lapply(c("ggplot2", "psych", "RCurl", "irr"), library, character.only=T)
 
 #Ana, please replace path below with the location of this file on your computer
-olecranon.data <- read.csv("/Users/rpietro/Google Drive/R/nonpublicdata_publications/OlecranonFracture/OlecranonFracture.csv")
+olecranon.data <- read.csv("/Users/user/Google Drive/Projeto_Duke/Olecranon/OlecranonFracture.csv", header=T)
 
 #checking data set as a whole
 attach(olecranon.data)
@@ -39,18 +39,21 @@ finn(olecranon_day1vsday30, 6, model="twoway") #Computes the Finn coefficient as
 icc(olecranon_day1vsday30, model="twoway", type="agreement") #we will use the default unit=single, no need to add that to the code. see documentation for details. Computes single score or average score ICCs as an index of interrater reliability of quantitative data. Additionally, F-test and confidence interval are computed. Bartko, J.J. (1966). The intraclass correlation coefficient as a measure of reliability. Psychological Reports, 19, 3-11. McGraw, K.O., & Wong, S.P. (1996), Forming inferences about some intraclass correlation co  efficients. Psychological Methods, 1, 30-46. Shrout, P.E., & Fleiss, J.L. (1979), Intraclass correlation: uses in assessing rater reliability. Psychological Bulletin, 86, 420-428.
 kappam.fleiss(olecranon_day1vsday30) #Computes Fleiss’ Kappa as an index of interrater agreement between m raters on categorical data. Conger, A.J. (1980). Integration and generalisation of Kappas for multiple raters. Psychological Bulletin, 88, 322-328. Fleiss, J.L. (1971). Measuring nominal scale agreement among many raters. Psychological Bul- letin, 76, 378-382. Fleiss, J.L., Levin, B., & Paik, M.C. (2003). Statistical Methods for Rates and Proportions, 3rd Edition. New York: John Wiley & Sons.
 
-#Joao, can you get below to work with the variables we have?
+#The function below is set for the example of olecranon_day1 Vs olecranon_day30. It will give the blandaltman plot.
 bland_altman_plot <- function(x,y,xlab="Average testresult", ylab="Deviation of experimental test")
   {
- x <- #var1, Ana here you should place the first observation
- y <- #var1, Ana here you should place the second observation
- d <- ((X + y)/2)
- diff <- x - y         
-
- plot(diff ~ d,pch=16,ylim=c(-6,6),xlab=xlab,ylab=ylab)
+ x <- olecranon_day1  #var1, Ana here you should place the first observation to be compared
+ y <- olecranon_day30 #var2, Ana here you should place the second observation to be compared
+ d <- ((olecranon_day1 + olecranon_day30)/2) # Here the functino is calculating the avareg value of the paired observations, so just change the formule according to this sequence (1o observation + 2o Observation)
+ diff <- x - y  # This is calculating the difference between paired observations       
+ #The plot command will give you the blanaltamn plot. 
+ #The "ylim" cammand sets the limit for y axis, here is set to -6 to 6, but you can change if you want to ge a broader range.
+ #The "xlim"  command sets the limit for x axis.
+ plot(diff ~ d,pch=16,ylim=c(-6,6),xlim=c(1,6),xlab=xlab,ylab=ylab)
  abline(h=mean(diff)-c(-1,0,1)*sd(diff),lty=2)
+ # The abline command will define the range for the agreement interval. Here it is set to 1 Standard Deviation + and 1 -, but you can change the range. See for genral information http://goo.gl/sv4kd and here http://goo.gl/IUbSJ for theoretical information about the plot. 
 }
-bland_altman_plot(d,diff,xlab="Avarage #Name of the variables",ylab="Difference Before and After #Name of the variable")
+bland_altman_plot(d,diff,xlab="Avarage",ylab="Difference Day1 to Day30")
 
 #######################################################################################
 #olecranonFracture.R is licensed under a Creative Commons Attribution - Non commercial 3.0 Unported License. You are free: to Share — to copy, distribute and transmit the work to Remix — to adapt the work, under the following conditions: Attribution — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work). Noncommercial — You may not use this work for commercial purposes. With the understanding that: Waiver — Any of the above conditions can be waived if you get permission from the copyright holder. Public Domain — Where the work or any of its elements is in the public domain under applicable law, that status is in no way affected by the license. Other Rights — In no way are any of the following rights affected by the license: Your fair dealing or fair use rights, or other applicable copyright exceptions and limitations; The author's moral rights; Rights other persons may have either in the work itself or in how the work is used, such as publicity or privacy rights. Notice — For any reuse or distribution, you must make clear to others the license terms of this work. The best way to do this is with a link to this web page. For more details see http://creativecommons.org/licenses/by-nc/3.0/
